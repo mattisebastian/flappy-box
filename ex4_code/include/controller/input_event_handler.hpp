8ddfd04fa6ed1c3dd8e9e42dc91306ad40239d2d
Vlad Vranceanu <v.vlad@gmx.at>
2014-06-13 10:31:57 +0200

    [input_event_handler.hpp] handle-function + struct keyboard_event

--- ex4_code/include/controller/input_event_handler.hpp
+++ ex4_code/include/controller/input_event_handler.hpp
@@ -9,9 +9,15 @@ namespace controller
 
       struct keyboard_event
       {
+	char key;
+        int modifier_mask;
+        double mouse_pos [2];
+	enum modifier_mask_enum {SHIFT_ACTIVE=1, CTRL_ACTIVE=2, ALT_ACTIVE=4};
+	
       };
 
     // TODO 4.1: declare pure-virtual handle function and define struct keyboard_event
+      virtual bool handle(keyboard_event const&) = 0;
 
   }; // InputEventHandler
 
